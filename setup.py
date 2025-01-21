f8be6a0e4ee4777523dbcc6d2fc974f19a81a367
dfm18 <dfm18@users.noreply.github.com>
2025-01-21 08:09:25 -0500

    change package name to 'exchange-rate-client'

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(
-    name="exchange_rate_client",
+    name="exchange-rate-client",
     version="0.1.0",
     packages=["exchange_rate_client"],
     install_requires=[
