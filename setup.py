b2a6ca8c04d6a8431024a9bfe961bf1fe758c6b9
dfm18 <dfm18@users.noreply.github.com>
2025-01-21 07:39:23 -0500

    fix for tests

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="exchange_rate_client",
+    version="0.1.0",
+    packages=["exchange_rate_client"],
+    install_requires=[
+        "requests>=2.0"
+    ]
+)
