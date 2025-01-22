9ac70a187305c415d36bb08ad1a460c8ecfbf6d4
dfm18 <dfm18@users.noreply.github.com>
2025-01-22 14:24:12 -0500

    export exceptions module

--- exchange_rate_client/__init__.py
+++ exchange_rate_client/__init__.py
@@ -5,9 +5,12 @@ __all__ = [
     "APIQuotaStatus",
     "Currency",
     "ExchangeRateV6Client",
+    "exceptions",
 ]
 
 
 from .commons import StandardResponse, PairConversion, HistoricalData, APIQuotaStatus
 
 from ._client import ExchangeRateV6Client
+
+from . import exceptions
